b4b84627d0bfb22e0090eaf816b995c82a36d3bb
EduardoFisher <44484160+EduardoFisher@users.noreply.github.com>
2020-05-01 12:07:03 -0700

    update loop for file reading.

--- FileReader.py
+++ FileReader.py
@@ -80,8 +80,8 @@ def calculateValues(lineOfValues, timeValues):
     # [6] = CopX
     # [7] = CopY
     
-    print(len(lineOfValues))
-    print(len(timeValues))
+    #print(len(lineOfValues))
+    #print(len(timeValues))
     displacementX = []
     displacementY = []
     velocityX = []
@@ -117,142 +117,145 @@ if os.path.isdir(path) == False:
 txtfiles = []
 for file in glob.glob("*.txt"):
     txtfiles.append(file)
+    print(file)
 
-newFileName = []
-for Tfile in txtfiles:
-    newFileName.append(Tfile.replace('.txt', '_CalculatedFile.txt'))
-# We probably want to start transtioning when 
-# Open each file.
-f = open(txtfiles[0], "r")
-#f = open("BeforeDemoTest001Just a test.txt", "r") #Opens each file we are going to work with. 
-# Read from the file line by line.
-f1 = f.readlines()
-mainList = []# We will be doing all of our calculations on this list.
-timeList = [] # We will hold all the times on here.
-timeSubList = [] 
-subList = []
-maxApCopX, maxMLCopY = 0, 0
-# Go throught each of the lines and using the Regex below to parse each string. 
-for line in f1:
-    res = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-    # Convert each number in from our regular expression from a string back into a float.
-    for index in range(len(res)):
-        # Convert the regex from string back into a float number so we can work with it.
-        res[index] = float(res[index])
-    # Currently we only want lines that have 1.0 in the title.
-    if len(res) != 0 and res[0] == 1.0:
-        # Append res to our subList.
-        res.pop(0)
-        timestamp = res.pop(0)
-        timeSubList.append(timestamp)
-        # Calculate COPX and COPY before hand and place it in the subList.
-        copx, copy = calcCop(res[3], res[4], res[2])
-        maxApCopX = max(maxApCopX, copx)
-        maxMLCopY = max(maxMLCopY, copy)
-        res.append(copx) # COPX at position [6], -My/Fz
-        res.append(copy)  # COPY at position [7], Mx/Fz
-        subList.append(res)
-    # When we are finally see a 2.0 when can put everything we just put into subList to our mainList.
-    elif len(res) != 0 and res[0] == 2.0:
-        if len(subList) != 0:
-            # Putting a copy of it in mainList so when we clear it we don't delete the list.
-            mainList.append(list(subList))
-            timeList.append(list(timeSubList))
-            timeSubList.clear()
-            subList.clear()
-f.close()
+#newFileName = []
+#for Tfile in txtfiles:
+#    newFileName.append(Tfile.replace('.txt', '_CalculatedFile.txt'))
+for current_file in txtfiles:
+    newFileName = current_file.replace('.txt', '_CalculatedFile.txt')
+    # We probably want to start transtioning when 
+    # Open each file.
+    f = open(current_file, "r")
+    #f = open(txtfiles[0], "r")
+    #f = open("BeforeDemoTest001Just a test.txt", "r") #Opens each file we are going to work with. 
+    # Read from the file line by line.
+    f1 = f.readlines()
+    mainList = []# We will be doing all of our calculations on this list.
+    timeList = [] # We will hold all the times on here.
+    timeSubList = [] 
+    subList = []
+    maxApCopX, maxMLCopY = 0, 0
+    # Go throught each of the lines and using the Regex below to parse each string. 
+    for line in f1:
+        res = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+        # Convert each number in from our regular expression from a string back into a float.
+        for index in range(len(res)):
+            # Convert the regex from string back into a float number so we can work with it.
+            res[index] = float(res[index])
+        # Currently we only want lines that have 1.0 in the title.
+        if len(res) != 0 and res[0] == 1.0:
+            # Append res to our subList.
+            res.pop(0)
+            timestamp = res.pop(0)
+            timeSubList.append(timestamp)
+            # Calculate COPX and COPY before hand and place it in the subList.
+            copx, copy = calcCop(res[3], res[4], res[2])
+            maxApCopX = max(maxApCopX, copx)
+            maxMLCopY = max(maxMLCopY, copy)
+            res.append(copx) # COPX at position [6], -My/Fz
+            res.append(copy)  # COPY at position [7], Mx/Fz
+            subList.append(res)
+        # When we are finally see a 2.0 when can put everything we just put into subList to our mainList.
+        elif len(res) != 0 and res[0] == 2.0:
+            if len(subList) != 0:
+                # Putting a copy of it in mainList so when we clear it we don't delete the list.
+                mainList.append(list(subList))
+                timeList.append(list(timeSubList))
+                timeSubList.clear()
+                subList.clear()
+    f.close()
         
 
-#print(txtfiles)
-fw = open("TestParse.txt","w+")
-#print(len(mainList))
-for i in mainList:
-    fw.write("========================================================================================================================\n")
-    for j in i:
-        fw.write(str(j)+"\n")
-fw.close()
-#print("Done writing")
+    #Used only for debugging purposes only.
+    #print(txtfiles)
+    #fw = open("TestParse.txt","w+")
+    #print(len(mainList))
+    #for i in mainList:
+    #    fw.write("========================================================================================================================\n")
+    #    for j in i:
+    #        fw.write(str(j)+"\n")
+    #fw.close()
+    #print("Done writing")
 
-#for x in mainList:
-#    for j in x:
-#        print(j)
-#print(findSizeofData(mainList))
-#print(filterData(mainList))
-#print("Before Filtering:")
-#calculateValues(mainList)
-#print("After Filtering:")
-#filterData(mainList)
-displacementX = []
-displacementY = []
-velocityX = []
-velocityY = []
-pathLength = []
-maxVelocityX, maxVelocityY = 0, 0
-minVelocityX, minVelocityY = 0, 0
-meanVelocityX, meanVelocityY = 0, 0
-filteredList = filterData(mainList)
-index = 0
-for stage in filteredList:
-    displacementX, displacementY, velocityX, velocityY, pathLength = calculateValues(stage, timeList[index])
-    maxVelocityX = max(velocityX)
-    maxVelocityY = max(velocityY)
-    meanVelocityX = statistics.mean(velocityX)
-    meanVelocityY = statistics.mean(velocityY)
-    index = index + 1
-    break
-asbDisplacementX = [abs(value) for value in displacementX]
-asbDisplacementY = [abs(value) for value in displacementY]
-'''
-newlist = filterData(mainList)
-for x in newlist:
-    for j in x:
-        print(j)
-    print("========================================================================================================================\n")
-'''
-'''
-for x in timeList:
-    for j in x:
-        print(j)
-    print("========================================================================================================================\n")
-'''
-txtfiles = []
-for file in glob.glob("*.txt"):
-    txtfiles.append(file)
-#print(txtfiles)
+    #for x in mainList:
+    #    for j in x:
+    #        print(j)
+    #print(findSizeofData(mainList))
+    #print(filterData(mainList))
+    #print("Before Filtering:")
+    #calculateValues(mainList)
+    #print("After Filtering:")
+    #filterData(mainList)
+    displacementX = []
+    displacementY = []
+    velocityX = []
+    velocityY = []
+    pathLength = []
+    maxVelocityX, maxVelocityY = 0, 0
+    minVelocityX, minVelocityY = 0, 0
+    meanVelocityX, meanVelocityY = 0, 0
+    filteredList = filterData(mainList)
+    index = 0
+    for stage in filteredList:
+        displacementX, displacementY, velocityX, velocityY, pathLength = calculateValues(stage, timeList[index])
+        maxVelocityX = max(velocityX)
+        maxVelocityY = max(velocityY)
+        meanVelocityX = statistics.mean(velocityX)
+        meanVelocityY = statistics.mean(velocityY)
+        index = index + 1
+        break
+    asbDisplacementX = [abs(value) for value in displacementX]
+    asbDisplacementY = [abs(value) for value in displacementY]
+    '''
+    newlist = filterData(mainList)
+    for x in newlist:
+        for j in x:
+            print(j)
+        print("========================================================================================================================\n")
+    '''
+    '''
+    for x in timeList:
+        for j in x:
+            print(j)
+        print("========================================================================================================================\n")
+    '''
+    #txfiles = []
+    #for file in glob.glob("*.txt"):
+    #    txtfiles.append(file)
+    #print(txtfiles)
 
-#print("Time Length: ", len(timeList))
-#print("DisplacementX: ", len(displacementX))
-#print("absdisplacement :", len(asbDisplacementX))
-#[print(asbDisplacementX[i]) for i in range(len(asbDisplacementX))]
-completeName = os.path.join(path, newFileName[0])
-file1 = open(completeName, "w")
-file1.write("Time\t\t CoPx\t\t Distance(d)\t\t Abs Distance\t\t Velocity\n")
-for i in range(len(timeList[0])):
-    if i == 0:
-        file1.write(str(timeList[0][i]) + '\t' + str(round(filteredList[0][i][6], 4)) + '\n')
-    else:
-        file1.write(str(timeList[0][i]) + '\t' + str(round(filteredList[0][i][6], 4)) + '\t\t\t' +
-        str(round(displacementX[i-1], 4)) + '\t\t\t' + str(round(asbDisplacementX[i-1], 4)) +
-        '\t\t\t\t' + str(round(velocityX[i-1], 4)) +"\n")
+    #print("Time Length: ", len(timeList))
+    #print("DisplacementX: ", len(displacementX))
+    #print("absdisplacement :", len(asbDisplacementX))
+    #[print(asbDisplacementX[i]) for i in range(len(asbDisplacementX))]
+    completeName = os.path.join(path, newFileName)
+    file1 = open(completeName, "w")
+    file1.write("Time\t CoPx\t Distance(d)\t Abs Distance\t Velocity\n")
+    for i in range(len(timeList[0])):
+        if i == 0:
+            file1.write(str(timeList[0][i]) + '\t' +str(round(filteredList[0][i][6], 4)) + '\n')
+        else:
+            file1.write(str(timeList[0][i]) + '\t' + str(round(filteredList[0][i][6], 4)) + '\t' +
+            str(round(displacementX[i-1], 4)) + '\t' + str(round(asbDisplacementX[i-1], 4)) + '\t' + str(round(velocityX[i-1], 4)) + "\n")
 
-file1.write("Max Range is: " + str(max(asbDisplacementX)) + "\n")
-file1.write("Mean Range is: " + str(statistics.mean(asbDisplacementX)) + "\n")
-file1.write("Peak Velocity is: " + str(max(velocityX)) + "\n")
-file1.write("Mean Velocity is: " + str(statistics.mean(velocityX)) + "\n")
+    file1.write("Max Range is: " + str(max(asbDisplacementX)) + "\n")
+    file1.write("Mean Range is: " + str(statistics.mean(asbDisplacementX)) + "\n")
+    file1.write("Peak Velocity is: " + str(max(velocityX)) + "\n")
+    file1.write("Mean Velocity is: " + str(statistics.mean(velocityX)) + "\n")
 
-file1.write("\nTime\t\t CoPy\t\t Distance(d)\t\t Abs Distance\t\t Velocity\n")
-for i in range(len(timeList[0])):
-    if i == 0:
-        file1.write(str(timeList[0][i]) + '\t' + str(round(filteredList[0][i][7], 4)) + '\n')
-    else:
-        file1.write(str(timeList[0][i]) + '\t' + str(round(filteredList[0][i][7], 4)) + '\t\t\t' +
-        str(round(displacementY[i-1], 4)) + '\t\t\t' + str(abs(round(asbDisplacementY[i-1], 4))) +
-        '\t\t\t\t' + str(round(velocityY[i-1], 4)) +"\n")
-file1.write("Max Range is: " + str(max(displacementY)) + "\n")
-file1.write("Mean Range is: " + str(statistics.mean(displacementY)) + "\n")
-file1.write("Peak Velocity is: " + str(max(velocityY)) + "\n")
-file1.write("Mean Velocity is: " + str(statistics.mean(velocityY)) + "\n")
-file1.close()
+    file1.write("\nTime\t CoPy\t Distance(d)\t Abs Distance\t Velocity\n")
+    for i in range(len(timeList[0])):
+        if i == 0:
+            file1.write(str(timeList[0][i]) + '\t' + str(round(filteredList[0][i][7], 4)) + '\n')
+        else:
+            file1.write(str(timeList[0][i]) + '\t' + str(round(filteredList[0][i][7], 4)) + '\t' +
+            str(round(displacementY[i-1], 4)) + '\t' + str(abs(round(asbDisplacementY[i-1], 4))) + '\t' + str(round(velocityY[i-1], 4)) +"\n")
+    file1.write("Max Range is: " + str(max(displacementY)) + "\n")
+    file1.write("Mean Range is: " + str(statistics.mean(displacementY)) + "\n")
+    file1.write("Peak Velocity is: " + str(max(velocityY)) + "\n")
+    file1.write("Mean Velocity is: " + str(statistics.mean(velocityY)) + "\n")
+    file1.close()
 
 '''
 JUST AS NOTES FOR NOW:
